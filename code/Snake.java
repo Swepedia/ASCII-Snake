0a78be24954b58c00a6efa82e44e2b54239bfd6e
Maxwell Heeschen <gorpmaster@gmail.com>
2016-11-07 20:56:10 -0600

    Cleaned up the code. Got rid of unneeded comments.

--- code/Snake.java
+++ code/Snake.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 import java.awt.event.KeyEvent;
 import jline.Terminal;
 
-public class Snake 
-{
+public class Snake {
 
     static final int FRAMES_PER_SECOND = 10;
     static final int SKIP_TICKS = 1000 / FRAMES_PER_SECOND;
@@ -13,18 +12,13 @@ public class Snake
     static char border = '.';
     static char dir = 0;
 
-	public static void main(String[] args) throws IOException, InterruptedException
-	{
+	public static void main(String[] args) throws IOException, InterruptedException {
         //Opens a new console window to run the game in
         Runtime runtime = Runtime.getRuntime();
-        if(System.console() == null)
-        {
-            try
-            {
+        if(System.console() == null) {
+            try {
                 runtime.exec("cmd /c start cmd.exe");
-            }
-            catch(IOException e)
-            {
+            } catch(IOException e) {
                 System.out.println("IOException");
                 runtime.exit(1);
             }
@@ -32,19 +26,13 @@ public class Snake
         Console console = System.console();
 
         //Sets up the board with the border
-        for(int i = 0; i < board.length; i++)
-        {
+        for(int i = 0; i < board.length; i++) {
             board[0][i] = border;
             board[board.length - 1][i] = border;
             board[i][0] = border;
             board[i][board[0].length - 1] = border;
         }
 
-        //display();
-        //runtime.exit(0);
-	
-        long nextTick = System.currentTimeMillis();
-        int sleep = 0;
         boolean run = true;
         KeyEvent kb;
 
@@ -53,47 +41,25 @@ public class Snake
             display();
             Thread.sleep(SKIP_TICKS);
         }
-
-        
     }
-    public static void display() throws IOException, InterruptedException
-        {
-            /*try
-            {
-                Runtime.getRuntime().exec("cmd /c cls");
-            }
-            catch(IOException e)
-            {
-                System.out.println("IOException");
-                runtime.exit(1);
-            }*/
-
-            //Clears the console screen before printing the board to it
-            try
-            {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            }
-            catch(IOException e)
-            {
-                System.out.println("IOException");
-            }
-            catch(InterruptedException e)
-            {
-                System.out.println("InterruptedException");
-            }
-
-            //Prints the board to the console
-            for(int i = 0; i < board.length; i++)
-            {
-                for(int j = 0; j < board[0].length; j++)
-                {
-                    //board[i][j] = 48;
-                    System.out.print(board[i][j] + " ");
-                }
-                System.out.println();
-            }
-            System.out.println("Arrow keys to move. \'Ctrl + C\' to exit");
+    public static void display() throws IOException, InterruptedException {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch(IOException e) {
+            System.out.println("IOException");
+        } catch(InterruptedException e) {
+            System.out.println("InterruptedException");
         }
+
+        //Prints the board to the console
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Arrow keys to move. \'Ctrl + C\' to exit");
+    }
 
     public static void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
